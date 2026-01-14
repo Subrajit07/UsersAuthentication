8867b6b6ef6d71f0c9fc65691f752d8778ec7455
Subrajit07 <subrajitlenka1234@gmail.com>
2026-01-14 22:28:39 +0530

    commit from sub-branch

--- UserAuthentication/src/main/java/com/user/dto/MailParameters.java
+++ UserAuthentication/src/main/java/com/user/dto/MailParameters.java
@@ -1,12 +1,8 @@
 package com.user.dto;
 
-import java.util.List;
-
 import lombok.Builder;
 import lombok.NonNull;
+
 @Builder
-public record MailParameters(@NonNull String to,
-									                         @NonNull String subject,
-									                         @NonNull String body,
-									                         boolean isHtml) {
+public record MailParameters(@NonNull String to, @NonNull String subject, @NonNull String body, boolean isHtml) {
 }
